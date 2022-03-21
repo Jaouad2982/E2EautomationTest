8a6061e2d50f98fe3cd5e4a6f3750e3b6e6fe6bd
jaouad <Jadsarouti@outlook.com>
2022-03-21 00:45:30 -0400

    prop project initializing

---
+++ src/main/java/com/auto/PropInializer.java
@@ -0,0 +1,35 @@
+package com.auto;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+
+public class PropInializer{
+	
+	
+	public static FileInputStream file;
+	public static Properties prop;
+	public static Properties returnProp(String path) {
+		try {
+			file = new FileInputStream(path);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		prop = new Properties();
+		try {
+			prop.load(file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return prop;
+	}
+	
+	
+	public static String returnValuefromProp(String dataProp) {
+		return prop.getProperty(dataProp);
+	}
+}
---
+++ src/main/java/resources/Constant.java
@@ -0,0 +1,9 @@
+package resources;
+
+public class Constant {
+	private String path = "C:\\Users\\jd\\eclipse-workspace\\SeleniumAutomation\\E2Eautomation\\src\\main\\java\\resources\\data.properties";
+	public String returnVariable() {
+		return this.path;
+	}
+
+}
