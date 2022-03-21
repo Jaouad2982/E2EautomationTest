ba7d4a3eb7d6585566d4e6dbe653b4a5e2c953e8
jaouad <Jadsarouti@outlook.com>
2022-03-21 01:03:11 -0400

    Base class driver initializer

--- src/main/java/com/auto/BaseClass.java
+++ src/main/java/com/auto/BaseClass.java
@@ -1,5 +1,42 @@
 package com.auto;
 
-public class BaseClass {
+import java.util.Properties;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass extends PropInializer {
+	
+	public static WebDriver driver;
+	Properties prop = PropInializer.prop;
+	public static WebDriver driverInializer() {
+		
+		switch(PropInializer.returnValuefromProp("browser")){
+		case "chrome":
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			break;
+		case "firefox":
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			break;
+		case "edge":
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+			break;
+//		default :
+//			//
+			
+		}
+		return driver;
+	}
+	
+	
+	public static void closeDriver() {
+		if(!(driver == null)) driver.close();
+	}
 }
